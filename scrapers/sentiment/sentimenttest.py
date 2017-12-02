7ef161cfe6513d16caf8c1d640d2b36295abbbbc
kv19971 <kshitijverma999@hotmail.com>
2017-12-03 05:39:56 +0800

    sentiment analysis works

--- scrapers/sentiment/sentimenttest.py
+++ scrapers/sentiment/sentimenttest.py
@@ -1,41 +1,7 @@
-import urllib
-import http.client as httplib
-import json
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
-# **********************************************
-# *** Update or verify the following values. ***
-# **********************************************
+txt = "The house was huge"
+sia = SIA()
+res = sia.polarity_scores(txt)
 
-# Replace the accessKey string value with your valid access key.
-accessKey = 'e665d40156af4f95b4a39278903f4cfc'
-
-# Replace or verify the region.
-#
-# You must use the same region in your REST API call as you used to obtain your access keys.
-# For example, if you obtained your access keys from the westus region, replace
-# "westcentralus" in the URI below with "westus".
-#
-# NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
-# a free trial access key, you should not need to change this region.
-uri = 'southeastasia.api.cognitive.microsoft.com'
-path = '/text/analytics/v2.0/sentiment'
-
-def GetSentiment (documents):
-    "Gets the sentiments for a set of documents and returns the information."
-
-    headers = {'Ocp-Apim-Subscription-Key': accessKey}
-    conn = httplib.HTTPSConnection (uri)
-    body = json.dumps (documents)
-    conn.request ("POST", path, body, headers)
-    response = conn.getresponse ()
-    return response.read ()
-
-documents = { 'documents': [
-    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
-    { 'id': '2', 'language': 'en', 'text': 'box' }
-]}
-
-print('Please wait a moment for the results to appear.\n')
-
-result = GetSentiment (documents)
-print (json.dumps(json.loads(result), indent=4))
+print(res)
